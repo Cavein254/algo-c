ff44a3630c5c0253ce424b96934c1293972d3061
johnmuthua <caveomuthama@gmail.com>
2024-02-13 22:02:52 +0300

    more styles

--- area_circle.c
+++ area_circle.c
@@ -4,9 +4,9 @@
 program to compute the area of a circle
 **/
 //a = pi r*2
-double area_circle(double radius)
+double area_circle(double *radius)
 {
-  return 3.14 * radius * radius;
+  return 3.14 * (*radius) * (*radius);
 }
 int main(void)
 {
@@ -14,7 +14,7 @@ int main(void)
   radius = 12;
   printf("Enter the radius to calculate \n");
   scanf("%lf", &radius);
-  double area = area_circle(radius);
+  double area = area_circle(&radius);
   printf("The area of the circle is %f \n", area);
   
 }

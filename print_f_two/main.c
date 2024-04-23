993fa7eac2fde11b7dc7460af1168adcffd47dab
johnmuthua <caveomuthama@gmail.com>
2024-04-23 19:54:00 +0300

    add two

--- print_f_two/main.c
+++ print_f_two/main.c
@@ -1,31 +1,34 @@
 #include <stdio.h>
 #include <stdarg.h>
+#include <string.h>
 
-int max(int num_args, ...);
+void print(char *initial, ...);
 
 int main(void)
 {
-	int max_num = max(5,99,100,-2000,200,2);
-	printf("min_num : %d\n", max_num);
+	print("ddff", 23,12,2.1,4.2);
+	print("fdfd", 1.1,32,2.1,45);
 
 	return 0;
 }
 
-int max(int num_args, ...)
+void print(char *initial, ...)
 {
+	int num_args = strlen(initial);
 	va_list args;
-	va_start(args, num_args);
 
-	int min;
+	va_start(args, initial);
 
-	for (int i = 0; i < num_args; i++)
-	{
-		int x = va_arg(args,int);
-		if (i == 0) min = x;
-		else if ( x < min) min = x;
+	for(int i = 0; i < num_args; i++){
+		if(initial[i] == 'd')
+		{
+			int x = va_arg(args, int);
+			printf("%d\n",x);
+		} else if(initial[i] == 'f')
+		{
+			double x = va_arg(args, double);
+			printf("%f\n", x);
+		}
 	}
-
 	va_end(args);
-
-	return min;
 }
